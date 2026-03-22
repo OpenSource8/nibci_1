52bd5d1fe95bfa7c99f6a933a66f5e66f4c20b2f
OpenSource8 <lukas.springer@student.tugraz.at>
2026-03-22 17:44:21 +0100

    implemented laplacian and car, still looks bad

--- skeleton_exercise_1.py
+++ skeleton_exercise_1.py
@@ -11,6 +11,7 @@
 from scipy.io import loadmat
 import matplotlib.pyplot as plt
 import mne
+import numpy as np
 
 # -- Parameter Definitions --
 sfreq = 200 # Hz
@@ -70,11 +71,65 @@ def eeg_plot(data, ch_names, sfreq, title="EEG"):
         show=True
     )
 
-# TODO: Write a function to apply laplacian derivative
-...
+# Function to apply laplacian rereferencing
 
-# TODO: Write a function to apply CAR
-...
+def apply_laplacian(data, neighbors= neighbors, ch_names = ch_names, g_j = 0.5):
+    """applies laplacian re-referencing on the given data
+
+    Args:
+        data: EEG data, seperated into channels
+        neighbors (dict, optional): dict of every electrode as key and its list of neighbours as value. Defaults to neighbors.
+        ch_names (list, optional): list of recorded channels. Defaults to ch_names.
+        g_j (float, optional): weighing factor. Defaults to 0.5.
+
+    Returns:
+        EEG data that is laplacian re-referenced
+    """
+    
+    n_channels, n_samples = data.shape # get number of channels and samples from the data
+    data_lap = np.empty((n_channels, n_samples)) # generate an empty matrix for the rereferenced values to go into
+
+    for idx, ch_name in enumerate(ch_names): #iterate over channels
+        channel_data = data[idx] #select the recording of the current channel
+        channel_neighbours = neighbors[ch_name] #  get the list of neighbours for that channel
+
+        n_neighbours = len(channel_neighbours) # how many neighbours
+        neighbour_weigthed_sum = np.zeros(n_samples) # preallocate space for th ewieghted sum used for substraction
+
+        for channel in channel_neighbours: # iterate over neighbours
+            neighbour_idx = ch_names.index(channel) # get the index for the chosen channel name
+            neighbor_data = data[neighbour_idx] # get the recorded data for that index
+
+            neighbour_weigthed_sum += g_j * neighbor_data # sum up the recordings of all neighbours 
+            
+        channel_laplace_data = channel_data - ((1/n_neighbours) * neighbour_weigthed_sum) # substract the weighted average of the neighbours from the channel
+
+        data_lap[idx] = channel_laplace_data # insert the data into the preallocated matrix
+
+
+    return data_lap
+
+# Function to apply CAR
+
+def apply_car(data, ch_names):
+
+    n_channels, n_samples = data.shape # get number of channels and samples from the data
+    data_car = np.empty((n_channels, n_samples)) # generate an empty matrix for the rereferenced values to go into
+
+    all_weigthed_sum = np.zeros(n_samples) # preallocate space for th ewieghted sum used for substraction
+
+    for idx, _ in enumerate(ch_names): #iterate over channels
+        current_channel_data = data[idx] # get the recorded data for that index
+        all_weigthed_sum += current_channel_data # sum up the recordings of all neighbours 
+        common_average = ((1/n_channels) * all_weigthed_sum) # average over all channels
+
+    for idx, _ in enumerate(ch_names): #iterate over channels
+        channel_data = data[idx] #select the recording of the current channel
+        channel_car_data = channel_data - common_average # substract the common average from the channel
+        data_car[idx] = channel_car_data # insert the data into the preallocated matrix
+
+    return data_car
+
 
 def main():
     # Load Data
@@ -85,13 +140,31 @@ def main():
     data2_noisy = data_2['noisy_data2']*1e-6
     data2_clean = data_2['clean_data2']*1e-6
 
+
     # Visualize noisy data
-    eeg_plot(data1_noisy, ch_names=ch_names, sfreq=sfreq)
-    eeg_plot(data2_noisy, ch_names=ch_names, sfreq=sfreq)
+    # eeg_plot(data1_noisy, ch_names=ch_names, sfreq=sfreq, title="EEG 1 noisy")
+
+    eeg_plot(data2_noisy, ch_names=ch_names, sfreq=sfreq, title="EEG 2 noisy")
 
     # TODO: Apply spatial filters according to the assignment sheet
-    ...
     
+    
+    data1_lap = apply_laplacian(data1_noisy, neighbors, ch_names, 0.5)
+    data1_car = apply_car(data1_noisy, ch_names)
+
+    data2_lap = apply_laplacian(data2_noisy, neighbors, ch_names, 0.5)
+    data2_car = apply_car(data2_noisy, ch_names)
+
+    # eeg_plot(data1_lap, ch_names=ch_names, sfreq=sfreq, title="EEG 1 noisy - Laplacian re-referencing applied")
+    # eeg_plot(data1_car, ch_names=ch_names, sfreq=sfreq, title="EEG 1 noisy - Car re-referencing applied")
+    # eeg_plot(data1_clean, ch_names=ch_names, sfreq=sfreq, title="EEG 1 Clean")
+
+    eeg_plot(data2_lap, ch_names=ch_names, sfreq=sfreq, title="EEG 2 noisy - Laplacian re-referencing applied")
+    eeg_plot(data2_car, ch_names=ch_names, sfreq=sfreq, title="EEG 2 noisy - Car re-referencing applied")
+    eeg_plot(data2_clean, ch_names=ch_names, sfreq=sfreq, title="EEG 2 Clean")
+
+
+
 
 if __name__ == "__main__":
     main()
